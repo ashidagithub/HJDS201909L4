4a79f44ac89a5875bddfd27f1dbf9c7c0f024b46
Mike. Shi <ashida2016@outlook.com>
2019-09-27 23:12:27 +0800

    created

---
+++ practices/index.py
@@ -0,0 +1,51 @@
+# Index of practices
+
+'''
+Part I - List
+1) Create a empty list
+2) Append to list
+    2.1) One bye One
+    2.2) By '*'
+    2.3) By extend()
+    2.4) By for
+3) Joint 2 lists
+4) Lenth a list
+5) Pick out from list
+    5.1) Pick 1  ( By [index] )
+    5.2) Pick by for
+    5.3) If exist ( By 'in')
+6) Delete in list
+    6.1) Delete one
+    6.2) Empty a list  ( By 'clear()')
+
+Part II - Operaters of List
+    max()
+    min()
+    len()
+    ------
+    list( seq )
+    ---------
+    sort()
+    index()
+    copy()
+
+
+Part III - Tuple
+1) Create a tuple
+2) Joint 2 tuples
+3) Lenth a tuple
+4) Pick out from tuple
+5) Delete a tuple
+
+Part IV - Operaters of List
+    max()
+    min()
+    len()
+    ------
+    tuple(seq)
+
+
+
+
+
+'''
